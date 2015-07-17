a400f72cfd39b29d094b9385b7ad8998374f6986
Luke Merrett <lukeamerrett@gmail.com>
2015-07-17 23:35:46 +0100

    Added a setup script for setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'Luke Merrett'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="BadgerNote",
+    packages=find_packages(),
+    install_requires=['peewee'],
+    author="Luke Merrett",
+    author_email="lukeamerrett@gmail.com",
+    url='https://github.com/lukemerrett/BadgerNote',
+    license="GPL",
+)
