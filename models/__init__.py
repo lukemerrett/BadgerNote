8f647cdeba63479704901866f2bbc3a32fe609d6
Luke Merrett <lukeamerrett@gmail.com>
2015-07-17 23:46:53 +0100

    Added a note model

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,20 @@ __author__ = 'Luke Merrett'
 
 from peewee import *
 import settings
+from datetime import datetime
 
 db = SqliteDatabase(settings.sqlite_database_name, threadlocals=True)
 
 class BaseModel(Model):
     class Meta:
         database = db
+
+class Note(BaseModel):
+    """
+    Table holding the notes
+    """
+    # Peewee adds primary key fields automatically as "id = int (1,1)"
+    title = TextField()
+    body = TextField()
+    date_created = DateTimeField(default=datetime.utcnow())
+    date_modified = DateTimeField(default=datetime.utcnow())
