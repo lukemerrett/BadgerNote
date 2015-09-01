a26eafeaa347605e1b3339273eab96516f74144b
Luke Merrett <lukeamerrett@gmail.com>
2015-09-01 20:30:17 +0100

    Added the argparse dependency to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="BadgerNote",
     packages=find_packages(),
-    install_requires=['peewee'],
+    install_requires=['peewee', 'argparse'],
     author="Luke Merrett",
     author_email="lukeamerrett@gmail.com",
     url='https://github.com/lukemerrett/BadgerNote',
