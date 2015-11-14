287f4b2414484add0ba309c7fa380f0ddf9b8933
Luke Merrett <lukeamerrett@gmail.com>
2015-11-14 13:10:46 +0000

    Abstracted away shared setup and teardown logic

--- tests/operations_tests.py
+++ tests/operations_tests.py
@@ -4,13 +4,18 @@ import os
 import unittest
 import operations
 
-class TestOperations(unittest.TestCase):
+class OperationTest(object):
   def setUp(self):
     operations.setup_database(test_database_name)
   def tearDown(self):
-    os.remove(test_database_name)
-    pass
+    os.remove(test_database_name) 
 
+class TestOperations(unittest.TestCase, OperationTest):
+  def setUp(self):
+    OperationTest.setUp(self)
+  def tearDown(self):
+    OperationTest.tearDown(self)
+ 
   def test_create_new_note(self):
     pass
 
